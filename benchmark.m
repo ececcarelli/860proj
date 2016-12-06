0c4c2b0d003701dfb9663f49df473249ca0a25aa
Emanuele Ceccarelli <emanuelececcarelli1@dhcp-18-189-86-105.dyn.MIT.EDU>
2016-12-06 01:13:01 -0500

    Benchmark

--- benchmark.m
+++ benchmark.m
@@ -1,3 +1,4 @@
+
 N = 1000;
 sig_X = 10;
 sig_eps = 1;
@@ -6,3 +7,8 @@ d = size(W_true, 1);
 
 X = normrnd(zeros(N, d), sig_X);
 y = X*W_true + normrnd(zeros(N, 1), sig_X);
+
+opt = gurls_defopt('standard');
+lambdas = paramsel_loocvprimal(X, y, opt);
+opt.paramsel.lambdas = lambdas.lambdas;
+opt.newprop('t0',1);
---
+++ make_benchmark.m
@@ -0,0 +1,18 @@
+function [X, y, opt] = make_benchmark(N, W_true, sig_array, t0, epochs)
+d = size(W_true, 1);
+s_n = size(sig_array);
+X = zeros(N, d);
+noise = zeros(N, 1);
+for i=1:s_n
+    sig_X = sig_array(i);
+    X = X + normrnd(zeros(N, d), sig_X);
+    noise = noise + normrnd(zeros(N, 1), sig_X);
+end
+
+y = X*W_true + noise;
+
+opt = gurls_defopt('standard');
+lambdas = paramsel_loocvprimal(X, y, opt);
+opt.paramsel.lambdas = lambdas.lambdas;
+opt.newprop('t0',t0);
+opt.newprop('epochs',epochs);
