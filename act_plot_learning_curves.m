e97a2c7d44897caad61b786691de38a6377f3e72
Emanuele Ceccarelli <emanuelececcarelli1@dhcp-18-189-80-146.dyn.MIT.EDU>
2016-12-13 18:11:01 -0500

    a

--- act_plot_learning_curves.m
+++ act_plot_learning_curves.m
@@ -2,25 +2,23 @@ function [rslt] = act_plot_learning_curves(X,y,opt,algorithms,max_epochs,num_tri
 w_opt = ((X' * X) \ (X' * y))';
 min_err = evaluate_obj_fun(X, y, w_opt, 0);
 rslt = cell(size(algorithms,2), 1);
+figure
 for i =1:size(algorithms,2)
     cfr = run_algo(X, y, opt,algorithms(i),max_epochs, num_trials);
     rslt{i} = cfr;
     if use_log
-        plot(log(evaluate_obj_fun(X, y, rslt{i}{1}.Ws, 0) - min_err))
+        plot(rslt{i}{1}.gcounts,log(evaluate_obj_fun(X, y, rslt{i}{1}.Ws, 0) - min_err))
     else
-        plot(evaluate_obj_fun(X, y, rslt{i}{1}.Ws, 0) - min_err)
+        plot(rslt{i}{1}.gcounts,evaluate_obj_fun(X, y, rslt{i}{1}.Ws, 0) - min_err)
     end
+    hold on;
 end
-figure
-hold on;
-plot(log(evaluate_obj_fun(X, y, rslt{1}{1}.Ws, 0) - min_err))
-plot(log(evaluate_obj_fun(X, y, rslt{2}{1}.Ws, 0) - min_err))
-plot(log(evaluate_obj_fun(X, y, rslt{3}{1}.Ws, 0) - min_err))
-hold off;
+
 
 title(title_str)
-xlabel('Iterations (epochs)') % x-axis label
-ylabel('Function value') % y-axis label
+xlabel('Number of single gradient computations') % x-axis label
+ylabel('Function suboptimality') % y-axis label
+xlim([0, min([rslt{1}{1}.gcount,rslt{2}{1}.gcount,rslt{3}{1}.gcount])])
 legend(algorithms,'Location','northeast')
 
 end
